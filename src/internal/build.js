fec7b16678f741c64dac8a714599f71398f66e73
ibrahimsunday <ibrahimsundayijai@gmail.com>
2025-01-11 01:39:57 +0100

    updated testnet tokens, mainnet coming

--- src/internal/build.js
+++ src/internal/build.js
@@ -1,23 +1,22 @@
 const { version } = require("../../package.json");
 const { sortTokens } = require("../utils/builder");
-const base = require("../tokens/base.json");
-const optimism = require("../tokens/optimism.json");
-const sepolia = require("../tokens/sepolia.json");
+const base_sepoli = require("../tokens/base-sepolia.json");
+const lisk_sepolia = require("../tokens/lisk-sepolia.json");
 
 module.exports = function buildList() {
   const parsed = version.split(".");
   return {
-    name: "Koan Protocol TokenList",
-    timestamp: new Date().toISOString(),
-    version: {
-      major: +parsed[0],
-      minor: +parsed[1],
-      patch: +parsed[2],
-    },
-    tags: {},
-    logoURI:
-      "https://raw.githubusercontent.com/koan-protocol/tokenlist/list/logos/koanlogo-256x256.png",
-    keywords: ["Koanprotocol", "default"],
-    tokens: sortTokens([...base, ...optimism, ...sepolia]),
-  };
+		name: "Koan Protocol TokenList",
+		timestamp: new Date().toISOString(),
+		version: {
+			major: +parsed[0],
+			minor: +parsed[1],
+			patch: +parsed[2],
+		},
+		tags: {},
+		logoURI:
+			"https://raw.githubusercontent.com/koan-protocol/tokenlist/list/logos/koanlogo-256x256.png",
+		keywords: ["Koanprotocol", "default"],
+		tokens: sortTokens([...base_sepoli, ...lisk_sepolia,]),
+	};
 };
