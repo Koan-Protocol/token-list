897f88d31267c81b21ff26e368d1523e9113bd9d
ibrahimijai <sunday.m1701072@st.futminna.edu.ng>
2025-02-26 01:30:42 +0100

    remove unnecessary logo, added new pngs, refactor logo folder

--- apis/src/index.ts
+++ apis/src/index.ts
@@ -16,7 +16,7 @@ app.use(
 		origin: [
 			"http://localhost:3000",
 			"http://localhost:5173",
-			"https://koanprotocol.xyz/",
+			"https://koanprotocol.xyz",
 		],
 		allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 		allowHeaders: ["Content-Type", "Authorization"],
